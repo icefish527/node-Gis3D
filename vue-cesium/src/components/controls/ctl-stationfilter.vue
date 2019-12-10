910e6e00aca2405ceb630d22496172f039528b94
郑杰 <zhengjie@lksoft.com.cn>
2019-12-10 15:01:54 +0800

    checkbox

---
+++ vue-cesium/src/components/controls/ctl-stationfilter.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div>
+      <el-button type="text" @click="dialog.dialogVisible = true">filter</el-button>
+    </div>
+
+    <el-dialog
+      v-if="dialog.dialogVisible"
+      v-dialogDrag:{dialogDrag}="dialog.dialogDrag"
+      v-dialogChange:{dialogChange}="dialog.dialogChange"
+      ref="dialog__wrapper"
+      :close-on-click-modal="false"
+      :title="dialog.title"
+      :visible.sync="dialog.dialogVisible"
+      :before-close="handleClose"
+    >
+      <div class="dialog-body">
+        <div class="line">
+          <ctl-StationFilter-ByCategory></ctl-StationFilter-ByCategory>
+        </div>
+      </div>
+      <slot slot="footer" class="dialog-footer"></slot>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import ctlStationFilterByCategory from "./ctl-stationfilterbycategory";
+Vue.component("ctl-StationFilter-ByCategory", ctlStationFilterByCategory);
+export default {
+  data() {
+    return {
+      selectedCity: [],
+      dialog: {
+        // dialog显示隐藏
+        dialogVisible: false,
+        dialogDrag: true, // 可拖拽
+        dialogChange: true, // 可拉伸
+        title: "详情"
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- vue-cesium/src/components/header.vue
+++ vue-cesium/src/components/header.vue
@@ -1,17 +1,22 @@
 <template>
   <div style="width:100% :after">
-    <div style="float:left;">
+    <div style="float:left;display:inline-block">
       <img src="../assets/logo.png" style="height:50px;margin-left:25px;" />
+      <div style="display:inline-block">
+        <ctl-menu></ctl-menu>
+      </div>
     </div>
     <div style="float:right;margin-right:200px;">
-      <ctl-menu></ctl-menu>
+      <ctl-Station-Filter></ctl-Station-Filter>
     </div>
   </div>
 </template>
 <script>
 import Vue from "vue";
 import ctlMenu from "./controls/ctl-menu";
+import ctlStationFilter from "./controls/ctl-stationfilter";
 Vue.component("ctl-menu", ctlMenu);
+Vue.component("ctl-Station-Filter", ctlStationFilter);
 export default {
   data() {
     return {};
